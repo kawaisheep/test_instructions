418d7cea8ff43a01e9f2c9eb45adc742d468cb46
kawaisheep <90024400+kawaisheep@users.noreply.github.com>
2026-03-27 14:15:35 +0200

    Update app.py

--- static/app.py
+++ static/app.py
@@ -5,8 +5,6 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
-
-# Віддача статичних файлів з папки static/
 @app.route("/static/<path:filename>")
 def static_files(filename):
     return send_from_directory('static', filename)
